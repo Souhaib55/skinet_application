6290a1b4cbd606483f069b9884c00354005a30db
Souhaib <souhaib.ammani55@gmail.com>
2025-03-05 21:06:49 +0100

    End of Section 15

--- skinet/client/src/app/layout/header/header.component.ts
+++ skinet/client/src/app/layout/header/header.component.ts
@@ -2,20 +2,34 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatBadgeModule } from '@angular/material/badge';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
+import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule, MatButtonModule, MatBadgeModule , RouterLink , RouterLinkActive , MatProgressBar],
+  imports: [MatIconModule, MatButtonModule, MatBadgeModule , RouterLink , RouterLinkActive , MatProgressBar , MatMenuTrigger , MatMenu , MatDivider , MatMenuItem],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   busyService = inject(BusyService);
   cartService = inject(CartService);
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  logout() {
+    this.accountService.logout().subscribe({
+      next: () => {
+        this.accountService.currentUser.set(null);
+        this.router.navigateByUrl('/');
+      }
+    });
+  }
 }
 
